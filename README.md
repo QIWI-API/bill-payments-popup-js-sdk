c7077728526802f99ea23447d41d17238420f49e
Artem Krinitsyn <a.krinitsyn@qiwi.com>
2018-12-04 10:47:56 +0300

    init webpack babel config

---
+++ README.md
@@ -0,0 +1,1 @@
+# Qiwi Checkout Popup
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                }
+            }
+        ]
+    },
+    devServer: {
+        contentBase: path.resolve(__dirname)
+    },
+};
