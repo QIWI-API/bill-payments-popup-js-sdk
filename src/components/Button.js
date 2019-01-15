eb236380925546fe69f36f02c0b13419ce783450
Artem Krinitsyn <a.krinitsyn@qiwi.com>
2019-01-15 12:18:28 +0300

    change button color

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,8 +1,7 @@
 import styled from 'styled-components'
 
 
-
-export const PrimaryButton = styled.button`
+export const Button = styled.button`
     font-family: 'Helvetica Neue', sans-serif;
     border: 1px solid rgba(0, 0, 0, 0.28);
     font-size: 16px;
@@ -27,3 +26,30 @@ export const PrimaryButton = styled.button`
         background-color: #e8e8e8;
     }
 `
+
+export const PrimaryButton = styled.button`
+    font-family: 'Helvetica Neue', sans-serif;
+    border: none;
+    font-size: 16px;
+    font-weight: 500;
+    text-align: center;
+    height: 48px;
+    min-width: 205px;
+    padding: 0 30px;
+    cursor: pointer;
+    border-radius: 24px;
+    color: #FFF;
+    transition: all 300ms;
+    background-color: #ff8c00;
+    outline: none
+
+    &:hover {
+        background-color: #ff8200;
+    }
+
+    &:active {
+        color: #fff1e3;
+        background-color: #ff8200;
+    }
+`
+
