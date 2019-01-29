f7117e774b88fc96d26241da906fc36ef5243a16
Artem Krinitsyn <a.krinitsyn@qiwi.com>
2019-01-29 11:35:06 +0300

    add rounded corners for popup

--- src/components/CheckoutIframe.js
+++ src/components/CheckoutIframe.js
@@ -1,5 +1,14 @@
 import React from 'react';
+import styled from 'styled-components';
 import qs from 'qs';
+
+
+const IframeWrapper = styled.div`
+  border-radius: 10px;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+`
 
 const CHECKOUT_URL = process.env.CHECKOUT_URL;
 
@@ -7,13 +16,15 @@ const CheckoutIframe = ({page, queryParams}) => {
   const queryString = qs.stringify(queryParams);
 
   return (
-    <iframe
-      src={`${CHECKOUT_URL}/${page}?${queryString}&embedded=true&billref=popup`}
-      frameBorder="0"
-      width="100%"
-      height="100%"
-      scrolling="no"
-    />
+    <IframeWrapper>
+      <iframe
+        src={`${CHECKOUT_URL}/${page}?${queryString}&embedded=true&billref=popup`}
+        frameBorder="0"
+        width="100%"
+        height="100%"
+        scrolling="no"
+      />
+    </IframeWrapper>
   );
 };
 
